c5bd959d272e90123ab85b592f54a819ed699632
BlooMilk <nick.w.edwards88@gmail.com>
2020-11-02 20:04:00 -0500

    Added message for missing inputs, made the radio buttons fancy, cleaned up redundancies in Javascript

--- Web Dev Project 3/Project3.js
+++ Web Dev Project 3/Project3.js
@@ -1,59 +1,35 @@
-let result = 0;
-let number1 = 0;
-let number2 = 0;
-let output = "";
-
-function addNumbers() {
-  number1 = document.getElementById("firstNumber").valueAsNumber;
-  number2 = document.getElementById("secondNumber").valueAsNumber;
-  result = number1 + number2;
-  output = `The result of adding ${number1} and ${number2} is ${result}`;
-  document.getElementById("textOutput").innerHTML = output;
-}
-
-function subtractNumbers() {
-  number1 = document.getElementById("firstNumber").valueAsNumber;
-  number2 = document.getElementById("secondNumber").valueAsNumber;
-  result = number1 - number2;
-  output = `The result of subtracting ${number1} and ${number2} is ${result}`;
-  document.getElementById("textOutput").innerHTML = output;
-}
-
-function multiplyNumbers() {
-  number1 = document.getElementById("firstNumber").valueAsNumber;
-  number2 = document.getElementById("secondNumber").valueAsNumber;
-  result = number1 * number2;
-  output = `The result of multiplying ${number1} and ${number2} is ${result}`;
-  document.getElementById("textOutput").innerHTML = output;
-}
-
-function divideNumbers() {
-  number1 = document.getElementById("firstNumber").valueAsNumber;
-  number2 = document.getElementById("secondNumber").valueAsNumber;
-  if (number2 === 0) {
-    document.getElementById("textOutput").innerHTML =
-      "Please don't try to divide by 0, that would be bad";
-  } else {
-    result = number1 / number2;
-    output = `The result of dividing ${number1} and ${number2} is ${result}`;
-    document.getElementById("textOutput").innerHTML = output;
-  }
-}
-
 function doSomeMath() {
-  if (document.getElementById("radioAdd").checked) {
-    addNumbers();
+  let result = 0;
+  let output = "";
+  let number1 = document.getElementById("firstNumber").valueAsNumber;
+  let number2 = document.getElementById("secondNumber").valueAsNumber;
+  if (
+    document.getElementById("firstNumber").value.length == 0 ||
+    document.getElementById("secondNumber").value.length == 0
+  ) {
+    output = "Please provide two numbers";
+  } else if (document.getElementById("radioAdd").checked) {
+    result = number1 + number2;
+    output = `The result of adding ${number1} and ${number2} is ${result}`;
   } else if (document.getElementById("radioSubtract").checked) {
-    subtractNumbers();
+    result = number1 - number2;
+    output = `The result of subtracting ${number1} and ${number2} is ${result}`;
   } else if (document.getElementById("radioMultiply").checked) {
-    multiplyNumbers();
+    result = number1 * number2;
+    output = `The result of multiplying ${number1} and ${number2} is ${result}`;
   } else if (document.getElementById("radioDivide").checked) {
-    divideNumbers();
+    if (number2 === 0) {
+      output = "Please don't try to divide by 0, that would be bad";
+    } else {
+      result = number1 / number2;
+      output = `The result of dividing ${number1} and ${number2} is ${result}`;
+    }
   }
+  document.getElementById("textOutput").innerHTML = output;
 }
 
 function resetOutput() {
-  document.getElementById("textOutput").innerHTML = "";
+  document.getElementById("textOutput").innerHTML = "&zwnj;";
   document.getElementById("firstNumber").focus();
 }
 
